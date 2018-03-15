c6b1bd0cf79d779f80817b42f8167a566460755c
Vivek Gupta <vg19988@gmail.com>
2018-03-16 01:01:19 +0530

    My template

---
+++ template.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define EB emplace_back
+#define MP make_pair
+#define all(o) (o).begin(), (o).end()
+#define mset(m,v) memset(m,v,sizeof(m))
+#define fr(i,n) for(lli i=0;i<n;++i)
+#define rep(i,a,b) for(lli i=a;i<=b;++i)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+#define endl '\n'
+typedef long long int lli;
+typedef long double ld;
+typedef pair< lli,lli > ii;
+typedef vector<lli> vi;
+typedef vector<ii> vii;
+typedef vector<vi> graph;
+lli MOD=1000000007;long double EPS=1e-9;
+
+
+void pre(){
+
+}
+
+void solve(){
+
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	//freopen("in.txt","r",stdin);
+	//freopen("out.txt","w",stdout);
+	pre();
+	int t=1;
+	//cin>>t;
+	for(int i=1;i<=t;i++){
+		//cout<<"Case #"<<i<<": ";
+		solve();
+	}
+}
